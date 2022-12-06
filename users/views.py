4127615a8aa818b0bdf10cb3a6c240726ab129c2
yamy007 <yaroslav_9@icloud.com>
2022-12-06 17:10:50 +0100

    add registration add login

---
+++ users/views.py
@@ -0,0 +1,27 @@
+from .forms import UserRegistrationForm
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+
+
+
+
+
+def register(request):
+
+    if request.method != 'POST':
+
+        form = UserRegistrationForm()
+    else:
+   
+        form = UserRegistrationForm(data=request.POST)
+        
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.is_active = False
+            user.save()
+            activateEmail(request, user, form.cleaned_data.get('email'))
+            return redirect('/')
+
+    context = {'form': form}
+    return render(request, 'registration/register.html', context)
