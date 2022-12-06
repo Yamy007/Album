82a1a4453ed31dfb7f2eb9a1a51af45f67f9863f
yamy007 <yaroslav_9@icloud.com>
2022-12-06 18:09:49 +0100

    add  views

--- main/views.py
+++ main/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render ,redirect
-
+from django.contrib.auth.models import User
+from .model import Article
 # Create your views here.
 
 
 def users(request):
     return redirect("/")
+
+
+def index(request: object):
+    if request.user.is_authenticated:
+        # article = Article.object.filter()
+        pass
+        
+    else:
+        redirect('/users/login')
